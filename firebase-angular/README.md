78ac98fe730eeb0ddcea0e9513aaf2ff65949703
Anderson R Soares <anderson.r.soares@gmail.com>
2018-07-02 15:27:16 -0300

    teste react

--- firebase-angular/README.md
+++ firebase-angular/README.md
@@ -6,3 +6,5 @@ firebase serve --only hosting,functions  -> roda local
 firebase deploy publica a pagina
 
 npm install -g @angular/cli
+
+firebase serve --only hosting,functions -> roda local firebase deploy publica a pagina
---
+++ firebase-react/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+//import App from './App';
+//import getFacts from './facts';
+
+// getFacts().then(facts => {
+//     render(<App facts={facts} />, document.querySelector('#root'));
+// });
+
+
+render(<h1>Hello, world!</h1>, document.querySelector('#root'));
+
+// ReactDOM.render(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('root')
+// );
